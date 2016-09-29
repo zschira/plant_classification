55c72589aedc475384bd5754f62d7b2b84f56c22
Zach Schira <Zach Schira>
2016-09-28 18:19:10 -0600

    added svm classify script

---
+++ python/svm_classify.py
@@ -0,0 +1,80 @@
+from sklearn.svm import SVC
+import pandas as pd
+import re
+import numpy as np
+
+fileName = "C:/temp/neon_plants.csv"
+df = pd.read_csv(fileName, skiprows = 0)
+
+def getTarget(df):
+	species = df['taxonid']
+	labels, levels = pd.factorize(species)
+	y = labels
+	return y
+
+def getFeatures(df):
+	ident = 'nm_'
+	names = ['chm_height']
+	for column in df:
+		if re.match(ident, column):
+			names.append(column)
+	X = df[names]
+	X = X.as_matrix()
+	return X
+
+y = getTarget(df)
+X = getFeatures(df)
+
+def featureNorm(X):
+	X_shape = X.shape
+	X_norm = X
+	mu = np.mean(X, axis=1)
+	sigma = np.std(X, axis = 1)
+	for i in range(X_shape[1]):
+		X_norm[:,i] = (X[:,i] - mu[i])/sigma[i]
+	return X_norm
+
+def randomizeVals(X, y):
+	X_shape = X.shape
+	arr = np.column_stack((X, y))
+	np.random.shuffle(arr)
+	X = arr[:,0:X_shape[1]]
+	y = arr[:,-1]
+	return(X, y)
+
+def getSets(X, y):
+	train_stop = round(.6 * y.size)
+	cv_stop = y.size - round(.2 * y.size)
+
+	X_train = X[0:train_stop, :]
+	y_train = y[0:train_stop]
+	X_cv = X[train_stop + 1:cv_stop, :]
+	y_cv = y[train_stop + 1:cv_stop]
+	X_test = X[cv_stop + 1:-1, :]
+	y_test = y[cv_stop + 1:-1]
+	return(X_train, X_test, X_cv, y_train, y_test, y_cv)
+
+X = featureNorm(X)
+(X, y) = randomizeVals(X, y)
+(X_train, X_test, X_cv, y_train, y_test, y_cv) = getSets(X, y)
+
+def findParams(X_train, y_train, X_cv, y_cv):
+	accuracy = 0
+	params = np.array([.01, .03, .1, .3, 1, 3, 10, 30])
+	for i in range(params.size):
+		for j in range(params.size):
+			C = params[i]
+			gamma = params[j]
+			clf = SVC(C=C, gamma=gamma)
+			clf.fit(X_train, y_train)
+			temp_acc = clf.score(X_test, y_test)
+			if temp_acc > accuracy:
+				clf_ideal = clf
+				C_ideal = C
+				gamma_ideal = gamma
+				accuracy = temp_acc
+	print("C:", C_ideal, "gamma:", gamma_ideal)
+	return clf_ideal
+
+clf_ideal = findParams(X_train, y_train, X_cv, y_cv)
+print("Accuracy:", clf_ideal.score(X_test, y_test))
