fe735b223d4eaf84e0d162ac8585dc29ad86d8b9
Zach Schira <Zach Schira>
2016-09-28 21:38:48 -0600

    added comments/cleaned code

--- python/svm_classify.py
+++ python/svm_classify.py
@@ -1,3 +1,5 @@
+# Script to read neon plant data and classify plant species using an SVM 
+
 from sklearn.svm import SVC
 import pandas as pd
 import re
@@ -6,12 +8,14 @@ import numpy as np
 fileName = "C:/temp/neon_plants.csv"
 df = pd.read_csv(fileName, skiprows = 0)
 
+#Extract taxonid column and assign an integer to each unique species this becomes y vector
 def getTarget(df):
 	species = df['taxonid']
 	labels, levels = pd.factorize(species)
 	y = labels
 	return y
 
+#Extract all columns to be used as features in the svm
 def getFeatures(df):
 	ident = 'nm_'
 	names = ['chm_height']
@@ -25,6 +29,7 @@ def getFeatures(df):
 y = getTarget(df)
 X = getFeatures(df)
 
+#Normalize features
 def featureNorm(X):
 	X_shape = X.shape
 	X_norm = X
@@ -34,6 +39,7 @@ def featureNorm(X):
 		X_norm[:,i] = (X[:,i] - mu[i])/sigma[i]
 	return X_norm
 
+#Randomize row order
 def randomizeVals(X, y):
 	X_shape = X.shape
 	arr = np.column_stack((X, y))
@@ -42,10 +48,10 @@ def randomizeVals(X, y):
 	y = arr[:,-1]
 	return(X, y)
 
+#Seperate data into a training set, cross validation set and test set
 def getSets(X, y):
 	train_stop = round(.6 * y.size)
 	cv_stop = y.size - round(.2 * y.size)
-
 	X_train = X[0:train_stop, :]
 	y_train = y[0:train_stop]
 	X_cv = X[train_stop + 1:cv_stop, :]
@@ -58,6 +64,7 @@ X = featureNorm(X)
 (X, y) = randomizeVals(X, y)
 (X_train, X_test, X_cv, y_train, y_test, y_cv) = getSets(X, y)
 
+#optemize constant parameters of cost function on the cross validation set
 def findParams(X_train, y_train, X_cv, y_cv):
 	accuracy = 0
 	params = np.array([.01, .03, .1, .3, 1, 3, 10, 30])
@@ -65,7 +72,7 @@ def findParams(X_train, y_train, X_cv, y_cv):
 		for j in range(params.size):
 			C = params[i]
 			gamma = params[j]
-			clf = SVC(C=C, gamma=gamma)
+			clf = SVC(C=C, gamma=gamma, decision_function_shape='ovr')
 			clf.fit(X_train, y_train)
 			temp_acc = clf.score(X_test, y_test)
 			if temp_acc > accuracy:
@@ -76,5 +83,6 @@ def findParams(X_train, y_train, X_cv, y_cv):
 	print("C:", C_ideal, "gamma:", gamma_ideal)
 	return clf_ideal
 
+#check accuracy
 clf_ideal = findParams(X_train, y_train, X_cv, y_cv)
 print("Accuracy:", clf_ideal.score(X_test, y_test))
